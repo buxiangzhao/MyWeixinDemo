b4c7776b3b66a3fde42307ae9c0df45338e78191
Buxiangzhao <1016862070@qq.com>
2018-07-20 19:29:54 +0800

    第一次提提交

---
+++ app.js
@@ -0,0 +1,66 @@
+//app.js
+App({
+  // 生命周期函数-监听小程序初始化
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+    //获取用户位置
+    wx.getLocation({
+      type: 'wgs84',
+      success: (res) => {
+        this.globalData.latitude = res.latitude // 经度
+        console.log("经度--" + this.globalData.latitude);
+        this.globalData.longitude = res.longitude // 纬度
+        console.log("纬度--" + this.globalData.longitude);
+      }
+    })
+  },
+  // 生命周期函数-监听小程序显示
+  onShow:function(){
+
+  },
+  // 生命周期函数-监听小程序隐藏
+  onHide: function () {
+
+  },
+  // 错误监听函数
+  onError: function (msg) {
+    console.log("异常信息--"+msg);
+
+  },
+  globalData: {
+    userInfo: null,
+    latitude: null,
+    longitude: null
+  },
+ 
+})
